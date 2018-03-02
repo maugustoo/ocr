ee32500371493ee0cca5ae3f739368568a986a5a
Marcelo Augusto <mekmay@hotmail.com>
2018-03-02 11:39:21 -0300

    Testing edit distance with some data.

--- src/java/ApplyOCR.java
+++ src/java/ApplyOCR.java
@@ -10,6 +10,10 @@ import org.apache.commons.lang.StringUtils;
 public class ApplyOCR {
 
     public static void main(String[] args) throws Exception {
+        String[] processos = {"000.621/2014-5", "002.964/2012-0", "005.867/2004-9", "008.213/2016-0", "010.450/1997-1",
+                "014.252/2015-5", "014.499/2017-7", "001.225/2014-6", "005.972/2015-9", "012.697/2017-6", "016.254/2015-5",
+                "020.690/2014-2", "001.208/2015-2", "018.590/2014-4", "019.501/2017-0", "022.094/2008-9", "025.968/2012-2",
+                "038.217/2012-0"};
 
         String dataDir = "E:\\Frames\\";
         // String dataDir = "E:\\23-05-2017_Segunda_Camara_Ordinaria.mp4.frame\\";
@@ -47,11 +51,25 @@ public class ApplyOCR {
                 texto = texto.trim();
                 String[] tokens = texto.split("\n");
                 String[] time = fileName.split("[.]");
-                if (tokens.length == 3) {
+                if (tokens.length == 3 && tokens[0].replaceAll("[^0-9/.\n-]", "").length() >= 5) {
                     stringBuilder.append("{");
                     stringBuilder.append("\"tempo\": \"" + time[0] + "\",");
 
                     tokens[0] = tokens[0].replaceAll("[^0-9/.\n-]", "");
+
+                    int indiceMelhorDistancia = 0;
+                    int melhordistancia = 1000000;
+
+                    for (int i = 0; i < processos.length; i++) {
+                        int dist = EditDistance.distance(processos[i], tokens[0]);
+
+                        if (dist < melhordistancia) {
+                            melhordistancia = dist;
+                            indiceMelhorDistancia = i;
+                        }
+
+                    }
+
                     stringBuilder.append("\"processo\": \"" + tokens[0].trim() + "\",");
 
                     tokens[1] = tokens[1].replaceAll("[^a-zA-ZáàâãéèêíïóôõöúçñÁÀ ÃÉÈÍÏÓÔÕÖÚÇÑ0-9\n ]", "");
@@ -60,6 +78,8 @@ public class ApplyOCR {
                     tokens[2] = tokens[2].replaceAll("[^a-zA-ZáàâãéèêíïóôõöúçñÁÀ ÃÉÈÍÏÓÔÕÖÚÇÑ\n ]", "");
                     stringBuilder.append("\"relator\": \"" + tokens[2].trim() + "\"");
                     stringBuilder.append("}");
+
+                    stringBuilder.append(" -- dist: " + melhordistancia + " - " + processos[indiceMelhorDistancia]);
 
                     try {
                         FileWriter arq = new FileWriter("E:\\Frames\\resultado.txt", true);
@@ -81,6 +101,4 @@ public class ApplyOCR {
         }
     }
 }
-
-
 
